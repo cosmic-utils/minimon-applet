68e1ad70ef624160cb6b6dae2e646aec1ab4cf9f
quantenzitrone <general@dev.quantenzitrone.eu>
2025-11-17 00:00:39 +0100

    workaround for removing the units with degree for ringcharts
    this works because ° is 2 bytes wide, so any temperature with 2 or 3 digits will
    be reduced to its number.
    If the temperature is single digit (<= 9°C) the degree sign will stay. If the
    temperature is above 1000°R (282°C) the number will be wrong, but at that point
    the cpu/gpu is probably already dead.
    
    I think ideally the RingChart constructor should receive some number with unit
    object, and decide from the number if it should display the unit or not.

--- src/sensors/cputemp.rs
+++ src/sensors/cputemp.rs
@@ -248,8 +248,8 @@ impl Sensor for CpuTemp {
                 let latest = self.latest_sample();
                 let mut value = self.to_string();
 
-                // remove the C/F/K unit if there's not enough space
-                if value.len() > 3 {
+                // remove the °C/°F/°R/K unit if there's not enough space (assuming temp stays below 282°C = 1000°R)
+                while value.len() > 3 {
                     let _ = value.pop();
                 }
                 chart_container!(crate::charts::ring::RingChart::new(
@@ -289,8 +289,8 @@ impl Sensor for CpuTemp {
                 let latest = self.latest_sample();
                 let mut value = self.to_string();
 
-                // remove the C/F/K unit if there's not enough space
-                if value.len() > 3 {
+                // remove the °C/°F/°R/K unit if there's not enough space (assuming temp stays below 282°C = 1000°R)
+                while value.len() > 3 {
                     let _ = value.pop();
                 }
 
